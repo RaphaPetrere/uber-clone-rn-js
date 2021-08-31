c59a0523902ba363c9bee54335a395fa42faf36f
Raphael <raphael.petrere@etapaeducacional.onmicrosoft.com>
2021-08-31 20:48:38 -0300

    ✨ Setting up Redux and its Provider

---
+++ slices/navSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+}
+
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+})
+
+export const {
+  setOrigin,
+  setDestination,
+  setTravelTimeInformation
+} = navSlice.actions;
+
+//Selector is used to grab the data from the dataLayer
+//a good practice is create one for each state
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
