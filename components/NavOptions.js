d7d99fffccd7ad2cf50d66274cbd096b19cdb8b9
Raphael Cardoso <raphaelpetrere@gmail.com>
2021-09-01 15:21:43 -0300

    ✨ Implementing React Native Navigation / Stack option

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import { Icon } from 'react-native-elements'
@@ -19,13 +20,18 @@ const data = [
 ]
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
     <FlatList 
       data={data}
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({item}) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity 
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          onPress={() => navigation.navigate(item.screen)}
+        >
           <View>
             <Image 
               source={{ uri: item.image }}
